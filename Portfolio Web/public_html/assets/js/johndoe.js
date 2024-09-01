12c37e56393f4da41f535b1349eba6a73876e463
Bercerfk57 <126180928+Bercerfk57@users.noreply.github.com>
2024-09-01 14:49:00 +0100

    site portfolio

---
+++ Portfolio Web/public_html/assets/js/johndoe.js
@@ -0,0 +1,459 @@
+/*!
+=========================================================
+* JohnDoe Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+
+$(document).ready(function () {
+  $(".portfolio-link").on("click", function (event) {
+    event.preventDefault();
+    smoothScrollTo(this.hash, 100, 700)
+  });
+  $(".btn").on("click", function (event) {
+    event.preventDefault();
+    smoothScrollTo(this.hash, 50, 700)
+  });
+  $(".navbar .nav-link").on("click", function (event) {
+    event.preventDefault();
+    smoothScrollTo(this.hash, 35, 700)
+  });
+});
+
+function smoothScrollTo(hash, offset = 100, duration = 700) {
+  if (hash !== "") {
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - offset,
+      },
+      duration,
+      function () {
+        history.replaceState(null, null, hash);
+      }
+    );
+  }
+}
+
+
+
+
+document.getElementById("download_CV").addEventListener("click", function () {
+  const pdfurl = "./assets/CV_Capgemini.pdf";
+  const link = document.createElement("a");
+  link.href = pdfurl;
+  link.download = "CV_Capgemini.pdf";
+  link.click();
+});
+
+// protfolio filters
+$(window).on("load", function () {
+  var t = $(".portfolio-container");
+  t.isotope({
+    filter: ".new",
+    animationOptions: {
+      duration: 750,
+      easing: "linear",
+      queue: !1,
+    },
+  }),
+    $(".filters a").click(function () {
+      $(".filters .active").removeClass("active"), $(this).addClass("active");
+      var i = $(this).attr("data-filter");
+      return (
+        t.isotope({
+          filter: i,
+          animationOptions: {
+            duration: 750,
+            easing: "linear",
+            queue: !1,
+          },
+        }),
+        !1
+      );
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  initMap();
+});
+
+// Leaf
+function initMap() {
+  var map = L.map("map").setView([43.52977287361497, 5.458032848442829], 13);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+}
+
+function generatePortfolioItems(items, language) {
+  const portfolioContainer = document.createElement("div");
+  portfolioContainer.className = "portfolio-container";
+
+  items.forEach((item) => {
+    const colDiv = document.createElement("div");
+    colDiv.className = "col-md-6 col-lg-4 " + item.filter;
+
+    const portfolioItem = document.createElement("div");
+    portfolioItem.className = "portfolio-item";
+
+    const portfolioLink = document.createElement("a");
+    portfolioLink.href =  item.link
+    portfolioLink.className = "portfolio-link";
+
+    const img = document.createElement("img");
+    img.src = item.src;
+    img.className = "img-fluid";
+    img.alt =
+      "Download free bootstrap 4 admin dashboard, free bootstrap 4 templates";
+
+    const contentHolder = document.createElement("div");
+    contentHolder.className = "content-holder";
+
+    const imgPopup = document.createElement("a");
+    imgPopup.className = "img-popup";
+    imgPopup.href = item.src;
+
+    const textHolder = document.createElement("div");
+    textHolder.className = "text-holder";
+
+    const title = document.createElement("h6");
+    title.className = "title";
+    title.textContent = item[language].title;
+
+    const subtitle = document.createElement("p");
+    subtitle.className = "subtitle";
+    subtitle.textContent = item[language].comment;
+
+    textHolder.appendChild(title);
+    textHolder.appendChild(subtitle);
+
+    contentHolder.appendChild(imgPopup);
+    contentHolder.appendChild(textHolder);
+
+    portfolioItem.appendChild(portfolioLink)
+
+    portfolioLink.appendChild(img);
+    portfolioLink.appendChild(contentHolder);
+
+    colDiv.appendChild(portfolioItem);
+
+    portfolioContainer.appendChild(colDiv);
+  });
+
+  return portfolioContainer;
+}
+
+const items = [
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO5.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO4.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO2.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO1.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO3.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO6.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO7.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO8.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO12.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO10.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO11.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "Solidworks Design",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Conception Solidworks",
+      comment: "Modèle SolidWorks que j'ai créé basé sur des images de la culture pop."
+    },
+    src: "assets/imgs/CAO/CAO9.png",
+    filter: "CAO",
+    link: "#cao"
+  },
+  {
+    en: {
+      title: "MarketPlace for Qodly",
+      comment: "Developed a marketplace website for Qodly using TypeScript, React, Express with tRPC, and MongoDB."
+    },
+    fr: {
+      title: "MarketPlace pour Qodly",
+      comment: "Développement d'un site de marché pour Qodly en utilisant TypeScript, React, Express avec tRPC, et MongoDB."
+    },
+    src: "assets/imgs/Qodly.jpg",
+    filter: "web new",
+    link: "#qodly"
+  },
+  {
+    en: {
+      title: "Cashless card top-up station for galas",
+      comment: "Designed a CASH-LESS recharge station for galas using a Raspberry Pi, Python, and the Lydia API."
+    },
+    fr: {
+      title: "Station de recharge de carte CASHLESS pour gala",
+      comment: "Conception d'une station de recharge de carte CASHLESS pour les galas en utilisant un Raspberry Pi, Python, et l'API Lydia."
+    },
+    src: "assets/imgs/Borne.jpg",
+    filter: "software elec new",
+    link: "#borne"
+  },
+  {
+    en: {
+      title: "This site",
+      comment: "Created a website using HTML and JavaScript to showcase my projects, incorporating Bootstrap and the Leaflet API."
+    },
+    fr: {
+      title: "Ce site",
+      comment: "Création d'un site web utilisant HTML et JavaScript pour présenter mes projets, intégrant Bootstrap et l'API Leaflet."
+    },
+    src: "assets/imgs/Site.jpg",
+    filter: "web new",
+    link: "#site"
+  },
+  {
+    en: {
+      title: "A voting machine",
+      comment: "Designed a voting machine for the Arts et Métiers school using a Raspberry Pi, two touchscreens, Python with Tkinter, and an Excel-based database."
+    },
+    fr: {
+      title: "Une machine à voter",
+      comment: "Conception d'une machine à voter pour l'école Arts et Métiers en utilisant un Raspberry Pi, deux écrans tactiles, Python avec Tkinter, et une base de données basée sur Excel."
+    },
+    src: "assets/imgs/vote.jpg",
+    filter: "software new elec",
+    link: "#vote"
+  },
+  {
+    en: {
+      title: "Car rental site",
+      comment: "Implementation of a car rental website using HTML, PHP, and PhpMyAdmin."
+    },
+    fr: {
+      title: "Site de location de voitures",
+      comment: "Mise en place d'un site de location de voiture utilisant HTML, PHP et PHPmyAdmin"
+    },
+    src: "assets/imgs/Rent.jpg",
+    filter: "web",
+    link: "#rent"
+  },
+  {
+    en: {
+      title: "Optimized alarm clock",
+      comment: "Design of an optimized alarm clock based on sleep cycles using an Arduino board and data processing with Python."
+    },
+    fr: {
+      title: "Réveil optimisé",
+      comment: "Conception d'un réveil optimisé se basant sur les cicles du sommeil avec un carte arduino et un traitement des données sous pyton"
+    },
+    src: "assets/imgs/Reveil.png",
+    filter: "elec",
+    link: "#reveil"
+  },
+  {
+    en: {
+      title: "Accounting and quotation for IREM",
+      comment: "Creation of various tools for quote assistance and qualification management for IREM using Python and Tkinter."
+    },
+    fr: {
+      title: "Comptabilité et devis pour IREM",
+      comment: "Création e divers logicield d'aide au devis et à la gestiond de qualification pour IREM sous python et tkinter"
+    },
+    src: "assets/imgs/blog1.jpg",
+    filter: "software",
+    link: "#irem"
+  },
+  {
+    en: {
+      title: "Smart follow suitcase",
+      comment: "SolidWorks model that I created based on pop culture images."
+    },
+    fr: {
+      title: "Valise intelligente suivie",
+      comment: "Conception d'une valise autoguidée vers l'utilisateur par bluetooth en utilisant une carte arduino."
+    },
+    src: "assets/imgs/blog1.jpg",
+    filter: "elec",
+    link: "#valise"
+  },
+  {
+    en: {
+      title: "Kavita",
+      comment: "Setting up an intranet book reading site."
+    },
+    fr: {
+      title: "Kavita",
+      comment: "Mise en place d'un site de lecture de livres sur intranet."
+    },
+    src: "assets/imgs/blog1.jpg",
+    filter: "web software",
+    link: "#valise"
+  }
+];
+
+let portfolioDiv;
+
+if (document.title === "Portfolio YB FR") {
+  portfolioDiv = generatePortfolioItems(items, "fr");
+
+} else {
+  portfolioDiv = generatePortfolioItems(items, "en");
+}
+
+const parentDiv = document.getElementById("ItemParent");
+if (parentDiv) {
+  parentDiv.appendChild(portfolioDiv);
+} else {
+  console.error("Div parent introuvable");
+}
